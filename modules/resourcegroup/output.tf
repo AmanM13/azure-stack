a540530af2eae8f4de037d394ca5b78d0483faaa
Aman Manapure <54535154+AmanM13@users.noreply.github.com>
2020-12-31 01:00:38 +0530

    azure-stack

--- modules/resourcegroup/output.tf
+++ modules/resourcegroup/output.tf
@@ -1,7 +1,7 @@
 output "resource_group_name" {
-    value = azurerm_resource_group.tier.name
+    value = azurerm_resource_group.azure-stack-rs.name
 }
 
 output "location_id" {
-    value = azurerm_resource_group.tier.location
+    value = azurerm_resource_group.azure-stack-rs.location
 }
