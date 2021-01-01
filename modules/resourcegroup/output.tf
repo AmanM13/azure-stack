a09374a9728da835bfeb520ffceb551032b4d7d6
Aman Manapure <54535154+AmanM13@users.noreply.github.com>
2021-01-01 08:43:19 +0530

    azure-stack

--- modules/resourcegroup/output.tf
+++ modules/resourcegroup/output.tf
@@ -1,7 +1,9 @@
 output "resource_group_name" {
     value = azurerm_resource_group.azure-stack-rs.name
+    description = "Name of the resource group."
 }
 
 output "location_id" {
     value = azurerm_resource_group.azure-stack-rs.location
+    description = "Location id of the resource group"
 }
