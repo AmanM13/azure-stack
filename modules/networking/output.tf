ed15d169d65e3e6e6fb6f315282b4989b4dbb116
Aman Manapure <54535154+AmanM13@users.noreply.github.com>
2021-01-01 08:46:46 +0530

    azure-stack

--- modules/networking/output.tf
+++ modules/networking/output.tf
@@ -1,17 +1,21 @@
 output "network_name" {
   value = azurerm_virtual_network.vnet01.name
+  description = "Name of the Virtual network"
 }
 
 output "websubnet_id" {
   value = azurerm_subnet.web-subnet.id
+  description = "Id of websubnet in the network"
 }
 
 output "appsubnet_id" {
   value = azurerm_subnet.app-subnet.id
+  description = "Id of appsubnet in the network"
 }
 
 output "dbsubnet_id" {
   value = azurerm_subnet.db-subnet.id
+  description = "Id of dbsubnet in the network"
 }
 
 
